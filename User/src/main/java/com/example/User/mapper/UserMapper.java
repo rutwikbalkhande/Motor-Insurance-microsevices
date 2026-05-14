62040ea35c72a6a209f77cb40d9fe2ee680dc6e2
Rutwik Balkhande <rutwikbalkhande42@gmail.com>
2026-05-14 12:15:33 +0530

    add Apis Access Steps in Readme.md file

--- User/src/main/java/com/example/User/mapper/UserMapper.java
+++ User/src/main/java/com/example/User/mapper/UserMapper.java
@@ -6,13 +6,15 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel="spring", nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
-    UserDTO toDTO(User user);
+    //Entity -> DTO
+    UserDTO toDTO (User user);
 
-    User toUser(UserDTO dto);
+    //DTO -> Entity
+    User toUser(UserDTO userDto);
 
-    void updateUserFromDto(UserDTO dto,
-                           @MappingTarget User user);
+    // ✅ Partial updatepartial update
+    void updateUserFromDto(UserDTO dto, @MappingTarget User user);
 }
